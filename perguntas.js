8b7aed717adfcc9de03c8073aa8f16f943fa4139
@Ramon Jordao <00001089925992sp@al.educacao.sp.gov.br>
2024-11-28 18:10:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'esportes ',
+    'O que é um goleiro de futebol ',
+    'O goleiro de futebol tem a função de defender o gol '
+)
+
+criaCartao(
+    'Programação',
+    'O que é web design?',
+    'Profissonal que cria apss e sites combinando conhecimentos de design e programação'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que é verbo to be?',
+    'O verbo to be significa "ser" ou "estar" '
+    )   
+
+    criaCartao(
+        'fisica',
+        'o que e radiacao ',
+        ' radiacao e ondas eletromagneticas que se propagam com uma determinada velocidade '
+        )   
+
+        criaCartao(
+            'biologia',
+            'o que e cadeia alimentar ?',
+            'cadeia alimentar e uma sequencia de seres vivos na qual um serve de alimento para o outro  '
+            )   
+
+            
